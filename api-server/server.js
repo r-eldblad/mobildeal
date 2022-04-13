88947ad91a43951170140e84f2a839fd7c599004
Rasmus Eldblad <r.eldblad@protonmail.com>
2022-04-13 08:55:26 +0200

    This commit deletes node_modules and adds it to .gitignore

--- api-server/server.js
+++ api-server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
 
-app.listen(port);
+// Database connection
+const dbName = "mobildeal";
+const dbURI = `mongodb://localhost:27017/${dbName}`;
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Listening on port " + port);
