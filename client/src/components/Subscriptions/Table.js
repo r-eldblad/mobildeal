775f746a1b7726f4ad685ac3cb3e9fe94b2a1d8f
Rasmus Eldblad <r.eldblad@protonmail.com>
2022-04-19 22:36:34 +0200

    This commit creates a table

--- client/src/components/Subscriptions/Table.js
+++ client/src/components/Subscriptions/Table.js
@@ -1,50 +1,88 @@
+import './Table.css'
+
 const Table = () => {
     return (
         <>
             <table>
-                <tr>
-                    <th>Operatör</th>
-                    <th>Surfmängd</th>
-                    <th>Bindningstid</th>
-                    <th>Pris</th>
-                    <th>Actions</th>
-                </tr>
-                <tr>
-                    <td>Telia</td>
-                    <td>2GB</td>
-                    <td>24 mån</td>
-                    <td>99kr</td>
-                </tr>
-                <tr>
-                    <td>Telia</td>
-                    <td>2GB</td>
-                    <td>24 mån</td>
-                    <td>99kr</td>
-                </tr>
-                <tr>
-                    <td>Telia</td>
-                    <td>2GB</td>
-                    <td>24 mån</td>
-                    <td>99kr</td>
-                </tr>
-                <tr>
-                    <td>Telia</td>
-                    <td>2GB</td>
-                    <td>24 mån</td>
-                    <td>99kr</td>
-                </tr>
-                <tr>
-                    <td>Telia</td>
-                    <td>2GB</td>
-                    <td>24 mån</td>
-                    <td>99kr</td>
-                </tr>
-                <tr>
-                    <td>Telia</td>
-                    <td>2GB</td>
-                    <td>24 mån</td>
-                    <td>99kr</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Operatör</th>
+                        <th>Surfmängd</th>
+                        <th>Bindningstid</th>
+                        <th>Samtal</th>
+                        <th>Sms</th>
+                        <th>Pris</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Telia</td>
+                        <td>2GB</td>
+                        <td>24 mån</td>
+                        <td>Fria</td>
+                        <td>Fria</td>
+                        <td>99kr</td>
+                        <td>
+                            <button type="button">Beställ</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Telia</td>
+                        <td>2GB</td>
+                        <td>24 mån</td>
+                        <td>Fria</td>
+                        <td>Fria</td>
+                        <td>99kr</td>
+                        <td>
+                            <button type="button">Beställ</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Telia</td>
+                        <td>2GB</td>
+                        <td>24 mån</td>
+                        <td>Fria</td>
+                        <td>Fria</td>
+                        <td>99kr</td>
+                        <td>
+                            <button type="button">Beställ</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Telia</td>
+                        <td>2GB</td>
+                        <td>24 mån</td>
+                        <td>Fria</td>
+                        <td>Fria</td>
+                        <td>99kr</td>
+                        <td>
+                            <button type="button">Beställ</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Telia</td>
+                        <td>2GB</td>
+                        <td>24 mån</td>
+                        <td>Fria</td>
+                        <td>Fria</td>
+                        <td>99kr</td>
+                        <td>
+                            <button type="button">Beställ</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Telia</td>
+                        <td>2GB</td>
+                        <td>24 mån</td>
+                        <td>Fria</td>
+                        <td>Fria</td>
+                        <td>99kr</td>
+                        <td>
+                            <button type="button">Beställ</button>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </>
     )
