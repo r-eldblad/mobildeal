8bb841276a3211bd03736738aeba703c3fef7c0f
Rasmus Eldblad <r.eldblad@protonmail.com>
2022-04-20 19:27:18 +0200

    This commit makes the table responsive

--- client/src/components/Subscriptions/Table.js
+++ client/src/components/Subscriptions/Table.js
@@ -6,80 +6,92 @@ const Table = () => {
             <table>
                 <thead>
                     <tr>
-                        <th>Operatör</th>
-                        <th>Surfmängd</th>
-                        <th>Bindningstid</th>
-                        <th>Samtal</th>
-                        <th>Sms</th>
-                        <th>Pris</th>
+                        <th className="priority-1">Operatör</th>
+                        <th className="priority-2">Surfmängd</th>
+                        <th className="priority-3">Bindningstid</th>
+                        <th className="priority-4">Samtal</th>
+                        <th className="priority-5">Sms</th>
+                        <th className="priority-6">Pris</th>
                         <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Telia</td>
-                        <td>2GB</td>
-                        <td>24 mån</td>
-                        <td>Fria</td>
-                        <td>Fria</td>
-                        <td>99kr</td>
+                        <td className="priority-1">Telia</td>
+                        <td className="priority-2">2GB</td>
+                        <td className="priority-3">24 mån</td>
+                        <td className="priority-4">Fria</td>
+                        <td className="priority-5">Fria</td>
+                        <td className="priority-6">99kr</td>
                         <td>
-                            <button type="button">Beställ</button>
+                            <button className="read-more" type="button">
+                                Läs mer
+                            </button>
                         </td>
                     </tr>
                     <tr>
-                        <td>Telia</td>
-                        <td>2GB</td>
-                        <td>24 mån</td>
-                        <td>Fria</td>
-                        <td>Fria</td>
-                        <td>99kr</td>
+                        <td className="priority-1">Telia</td>
+                        <td className="priority-2">2GB</td>
+                        <td className="priority-3">24 mån</td>
+                        <td className="priority-4">Fria</td>
+                        <td className="priority-5">Fria</td>
+                        <td className="priority-6">99kr</td>
                         <td>
-                            <button type="button">Beställ</button>
+                            <button className="read-more" type="button">
+                                Läs mer
+                            </button>
                         </td>
                     </tr>
                     <tr>
-                        <td>Telia</td>
-                        <td>2GB</td>
-                        <td>24 mån</td>
-                        <td>Fria</td>
-                        <td>Fria</td>
-                        <td>99kr</td>
+                        <td className="priority-1">Telia</td>
+                        <td className="priority-2">2GB</td>
+                        <td className="priority-3">24 mån</td>
+                        <td className="priority-4">Fria</td>
+                        <td className="priority-5">Fria</td>
+                        <td className="priority-6">99kr</td>
                         <td>
-                            <button type="button">Beställ</button>
+                            <button className="read-more" type="button">
+                                Läs mer
+                            </button>
                         </td>
                     </tr>
                     <tr>
-                        <td>Telia</td>
-                        <td>2GB</td>
-                        <td>24 mån</td>
-                        <td>Fria</td>
-                        <td>Fria</td>
-                        <td>99kr</td>
+                        <td className="priority-1">Telia</td>
+                        <td className="priority-2">2GB</td>
+                        <td className="priority-3">24 mån</td>
+                        <td className="priority-4">Fria</td>
+                        <td className="priority-5">Fria</td>
+                        <td className="priority-6">99kr</td>
                         <td>
-                            <button type="button">Beställ</button>
+                            <button className="read-more" type="button">
+                                Läs mer
+                            </button>
                         </td>
                     </tr>
                     <tr>
-                        <td>Telia</td>
-                        <td>2GB</td>
-                        <td>24 mån</td>
-                        <td>Fria</td>
-                        <td>Fria</td>
-                        <td>99kr</td>
+                        <td className="priority-1">Telia</td>
+                        <td className="priority-2">2GB</td>
+                        <td className="priority-3">24 mån</td>
+                        <td className="priority-4">Fria</td>
+                        <td className="priority-5">Fria</td>
+                        <td className="priority-6">99kr</td>
                         <td>
-                            <button type="button">Beställ</button>
+                            <button className="read-more" type="button">
+                                Läs mer
+                            </button>
                         </td>
                     </tr>
                     <tr>
-                        <td>Telia</td>
-                        <td>2GB</td>
-                        <td>24 mån</td>
-                        <td>Fria</td>
-                        <td>Fria</td>
-                        <td>99kr</td>
+                        <td className="priority-1">Telia</td>
+                        <td className="priority-2">2GB</td>
+                        <td className="priority-3">24 mån</td>
+                        <td className="priority-4">Fria</td>
+                        <td className="priority-5">Fria</td>
+                        <td className="priority-6">99kr</td>
                         <td>
-                            <button type="button">Beställ</button>
+                            <button className="read-more" type="button">
+                                Läs mer
+                            </button>
                         </td>
                     </tr>
                 </tbody>
