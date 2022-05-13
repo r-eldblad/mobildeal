9f15489c4301dabc823f55907b53eaf6ce3f46f8
Rasmus Eldblad <r.eldblad@protonmail.com>
2022-05-13 12:12:37 +0200

    This commit implements PWA

---
+++ client/public/service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'version-1'
+const urlsToCache = ['index.html', 'offline.html', 'offline.css']
+
+const self = this
+
+// Installation SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache')
+            return cache.addAll(urlsToCache)
+        })
+    )
+})
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(() => {
+                return fetch(event.request).catch((err) => caches.match('offline.html'))
+            })
+            .then(() => {
+                return fetch(event.request).catch((err) => caches.match('offline.css'))
+            })
+    )
+})
+
+// Activate the service worker
+self.addEventListener('activate', (event) => {
+    const cacheWhiteList = []
+    cacheWhiteList.push(CACHE_NAME)
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhiteList.includes(cacheName)) {
+                        return caches.delete(cacheName)
+                    } else {
+                        return null
+                    }
+                })
+            )
+        )
+    )
+})
